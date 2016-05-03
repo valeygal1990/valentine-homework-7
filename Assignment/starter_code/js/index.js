7dc604892fbba37b119b5ae334716bdca8426638
valeygal1990 <valentinesakwa@Valentines-MacBook-Air.local>
2016-05-03 00:10:08 -0400

    Initial commit

---
+++ Assignment/starter_code/js/index.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+var cityImages = [
+	'images/austin.jpg',
+	'images/citipix_skyline.jpg',
+	'images/la.jpg',
+	'images/nyc.jpg',
+	'images/sf.jpg',
+	'images/sydney.jpg'
+
+
+];
+
+var currentIndex = 0;
+
+ //When you click the up or down button, display the drop down list of citites
+
+ //when you lick on a city, display the corresponding background
+
+ $('#city-type').click(displayImageSelections);
+
+ function displayImageSelections{
+
+	var action = $(this).val();
+
+	if (action = ""
+
+ 	// $('#city-type option:selected').text();
+
+}
+
+
+
+
+
+ 
+
+
+
+
+
+	
+});
